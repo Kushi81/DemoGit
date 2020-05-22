560fe53055f3a17fe7a9c238c92f7b57978b03b1
PC <PC@PC-PC>
2020-05-22 23:01:16 +0700

    First Commit

---
+++ GitPracticeDemo/src/main/java/StringPractice.java
@@ -0,0 +1,15 @@
+
+public class StringPractice {
+		public static void main(String[] args) {
+		String var="hello";
+		
+		System.out.println(var);
+		}
+		public static void main(double a) {
+		String var="jack";
+		
+		System.out.println(var);
+		}
+	}
+
+
